3a5a1ca8684840a4b3beced9b818dc069f5ca7c8
tieubao1711 <quangtran1711dn@gmail.com>
2024-11-20 00:53:14 +0700

    Update .gitignore

--- dist/config/allowedUsers.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedUsers = void 0;
-exports.allowedUsers = ['username1', 'username2']; // Thay bằng username thực tế
